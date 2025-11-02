dcab5c859850a0b0b13b3f84522f7a30b2b01f1f
oishee44 <anikaoishee44@gmail.com>
2025-11-02 13:26:58 +0600

    Implement game mode selection in Tic Tac Toe

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -17,3 +17,32 @@ void computerMove(char board[3][3]);
 int minimax(char board[3][3], int depth, int isMaximizing);
 int selectGameMode();
 void displayGameStats(int xWins, int oWins, int draws);
+
+
+
+int selectGameMode()
+{
+    clearScreen();
+    setColor(COLOR_YELLOW);
+    printf("\n=== TIC TAC TOE - SELECT MODE ===\n\n");
+    setColor(COLOR_RESET);
+    printf("1. Player vs Player\n");
+    printf("2. Player vs Computer\n");
+    printf("3. Back to Main Menu\n\n");
+    printf("Enter choice (1-3): ");
+
+    int choice;
+    if (scanf("%d", &choice) != 1)
+    {
+        clearInputBuffer();
+        return 1;
+    }
+    clearInputBuffer();
+
+    if (choice == 3)
+        return -1;
+    if (choice < 1 || choice > 3)
+        return 1;
+    return choice;
+}
+
